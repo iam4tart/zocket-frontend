b4d3efde5ddd41c7fcee105d8492de1fdbbe3f52
iamt4art <e21cseu0038@bennett.edu.in>
2024-07-27 00:17:10 +0530

    cleaning

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-// import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    // VitePWA({
-    //   includeAssets: ['logo.svg'],
-    //   registerType: 'autoUpdate',
-    //   workbox: {
-    //     globPatterns: ['**/*.{js,css,html,ico,png,svg}']
-    //   },
-    //   manifest: {
-    //     name: 'Zocket - Ad Customization',
-    //     short_name: 'AdCustomization',
-    //     description: 'Zocket tool for ad customization providing user with customizable ad templates',
-    //     theme_color: '#ffffff',
-    //   },
-    //   devOptions: {
-    //     enabled: true
-    //   }
-    // }),
   ],
 });
